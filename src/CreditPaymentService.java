a333b3605e8e1b9c7cf7a516a0661a74cdbc00e3
Ирина Ракова <irakova.education@gmail.com>
2023-10-13 17:53:55 +0300

    Changed2 commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,6 @@
 public class CreditPaymentService {
     public int calculate(int amount, double percent, int years) {
-        int months;//количество месяцев, в течение которых производятся выплаты
-        months = years * 12;
+        int months = years * 12;
         double percentToHundred = percent / 100;
         double repayment = amount * percentToHundred / 12 * Math.pow(1 + (percentToHundred / 12), months) / (Math.pow(1 + (percentToHundred / 12), months) - 1);
         return (int) repayment;
